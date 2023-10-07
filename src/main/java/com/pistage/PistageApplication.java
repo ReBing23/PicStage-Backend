8c26add5a3bea6337b2aad2c8e11b52facf54960
silver <silver36c@gmail.com>
2023-10-07 15:25:13 +0900

    chore: initialize

---
+++ src/main/java/com/pistage/PistageApplication.java
@@ -0,0 +1,13 @@
+package com.pistage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PistageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PistageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pistage/PistageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pistage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PistageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
